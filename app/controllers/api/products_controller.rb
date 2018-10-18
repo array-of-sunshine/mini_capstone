884a5c9bec9653ed5e1ff05555e600ddf38e4511
Brian <riceb53@gmail.com>
2018-10-18 14:55:38 -0500

    make RESTful routes and actions

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,12 +1,41 @@
 class Api::ProductsController < ApplicationController
+  def show
+    # get some data from the db, and show it to the user
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    render "show.json.jbuilder"
+  end
+
   def index
     @products = Product.all
     render "index.json.jbuilder"
   end
 
-  def show
-    # get ONE product from the db
-    @product = Product.find_by(id: params[:id])
+  def create
+    @product = Product.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+    @product.save
     render "show.json.jbuilder"
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.update(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+    render "show.json.jbuilder"
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    render json: {message: "You just deleted the item"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
   namespace :api do
-    get '/products' => 'products#index'
+   get '/products' => 'products#index'
     get '/products/:id' => 'products#show'
+    post '/products' => 'products#create'
+    patch '/products/:id' => 'products#update'
+    delete '/products/:id' => 'products#destroy'
   end
 end
