e9cab88047a11b3abf995e479cb60c215407ac5a
Brian <riceb53@gmail.com>
2018-10-21 12:25:30 -0500

    add sorting and filtering

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,4 +1,17 @@
 class Api::ProductsController < ApplicationController
+  def index
+    search_term = params[:user_input]
+    @products = Product.where('name LIKE ?', "%#{search_term}%")
+    if params[:sort] == 'price'
+      # sort by price
+      @products = @products.order(:price => :asc)
+    else
+      # sort by id
+      @products = @products.order(:id => :asc)
+    end
+    render "index.json.jbuilder"
+  end
+
   def show
     # get some data from the db, and show it to the user
     product_id = params[:id]
@@ -6,29 +19,33 @@ class Api::ProductsController < ApplicationController
     render "show.json.jbuilder"
   end
 
-  def index
-    @products = Product.all
-    render "index.json.jbuilder"
-  end
 
   def create
     @product = Product.new(
-      first_name: params[:first_name],
-      last_name: params[:last_name],
-      email: params[:email],
-      phone_number: params[:phone_number]
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description]
     )
-    @product.save
-    render "show.json.jbuilder"
+    # the save could work, the save could break
+    if @product.save
+      p "product saved"
+      # do something -- do the regular thing
+      render "show.json.jbuilder"
+    else
+      p 'product did not save'
+      # do some other thing, tell the user they made a mistake
+      render "errors.json.jbuilder"
+    end
   end
 
   def update
     @product = Product.find_by(id: params[:id])
     @product.update(
-      first_name: params[:first_name],
-      last_name: params[:last_name],
-      email: params[:email],
-      phone_number: params[:phone_number]
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description]
     )
     render "show.json.jbuilder"
   end
