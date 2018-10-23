52cae3053efd85acc5f7b7bee52c3bb728d2b790
Brian <riceb53@gmail.com>
2018-10-22 21:28:55 -0500

    add built in has_many and belongs_to

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,13 @@
 class Product < ApplicationRecord
+  belongs_to :supplier
+  # def supplier
+  #   Supplier.find_by(id: supplier_id)
+  # end
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :price, presence: true
   validates :price, numericality: { greater_than: 0 }
   validates :description, presence: true
-
-  def supplier
-    Supplier.find_by(id: supplier_id)
-  end
 
   def is_discounted?
     # return true if price is low
