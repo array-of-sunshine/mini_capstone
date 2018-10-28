c584830fa71e1d62cb964c51a49874ffaef1eba3
Brian <riceb53@gmail.com>
2018-10-28 10:40:07 -0500

    finish orders create and add orders index (filtered by current_user)

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,16 +1,31 @@
 class Api::OrdersController < ApplicationController
+  def index
+    # @orders = Order.where(user_id: current_user.id)
+    @orders = current_user.orders
+    render "index.json.jbuilder"
+  end
+
   def create
     # most basic create action of all time
-    p 'current_user'
-    p current_user
-    p 'current_user'
+    # subtotal
+    # subtotal == quantity * price
+    product = Product.find_by(id: params[:product_id])
+
+    price = product.price
+
+    calculated_subtotal = params[:quantity].to_i * price
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+
     @order = Order.new(
       product_id: params[:product_id],
-      # current_user
-      user_id: 1,
-      quantity: params[:quantity]
+      user_id: current_user.id,
+      quantity: params[:quantity],
+      subtotal: calculated_subtotal,
+      total: calculated_total,
+      tax: calculated_tax
     )
-    # @order.save
+    @order.save
     # how can i figure out why this isn't saving
     p @order.errors.full_messages
     render "show.json.jbuilder"
