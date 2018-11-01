8b7705be9280b7637671587d907204a43db409f6
Brian <riceb53@gmail.com>
2018-11-01 11:53:50 -0500

    make schema for shopping cart

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,3 @@
 class Order < ApplicationRecord
   belongs_to :user#, optional: true
-  belongs_to :product
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
 
     post '/orders' => 'orders#create'
     get '/orders' => 'orders#index'
+
   end
 end
